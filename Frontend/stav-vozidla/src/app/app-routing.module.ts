27d26aec8c922d6627fd829d532473a3066b7ec6
robert <robert@vangor.me>
2021-12-06 10:16:06 +0100

    Add customer interface

--- Frontend/stav-vozidla/src/app/app-routing.module.ts
+++ Frontend/stav-vozidla/src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {CustomerComponent} from "./customer/customer.component";
 import {LoginComponent} from "./login/login.component";
+import {OrdersTableComponent} from "./orders-table/orders-table.component";
+import {OrderComponent} from "./order/order.component";
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
