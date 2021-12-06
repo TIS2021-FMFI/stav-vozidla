8e9d6d37503bf4ce894d55b032b10041285b067c
Adam Gonšenica <adagon001@gmail.com>
2021-12-06 18:03:31 +0100

    fix

--- Backend/controllers/dataImporter.js
+++ Backend/controllers/dataImporter.js
@@ -3,12 +3,12 @@ const db = require('../models');
 const Sequelize = require('sequelize');
 
 module.exports.importData = (csvData) => {
-  csvData.forEach((csvrow) => {
+  csvData.forEach(async (csvrow) => {
     if (csvrow[0] != 'Final Consignee Code') {
       var completionDate =
         csvrow[6] == '' ? null : moment.utc(csvrow[6], 'DD.MM.YYYY HH:mm');
       var entryDate = moment.utc(csvrow[4], 'DD.MM.YYYY HH:mm');
-      db.Order.findOrCreate({
+      await db.Order.findOrCreate({
         where: { VIN: csvrow[3], entryDate: entryDate },
         // if not found :
         defaults: {
